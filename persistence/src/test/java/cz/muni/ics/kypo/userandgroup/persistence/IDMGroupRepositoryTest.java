152d1537ca07fdad2267a3237a152a3441f6b87f
Jan Duda <394179@mail.muni.cz>
2018-06-04 10:55:48 +0200

    Added application.properties for tests

--- persistence/src/test/java/cz/muni/ics/kypo/userandgroup/persistence/IDMGroupRepositoryTest.java
+++ persistence/src/test/java/cz/muni/ics/kypo/userandgroup/persistence/IDMGroupRepositoryTest.java
@@ -12,7 +12,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
