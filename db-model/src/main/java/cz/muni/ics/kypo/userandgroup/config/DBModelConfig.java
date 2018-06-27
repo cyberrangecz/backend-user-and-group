8e9137df40ae812077c1f482eb9d9daeb137429a
Jan Duda <394179@mail.muni.cz>
2018-06-27 10:44:33 +0200

    Removed application.properties files and loading all properties from external file

--- db-model/src/main/java/cz/muni/ics/kypo/userandgroup/config/DBModelConfig.java
+++ db-model/src/main/java/cz/muni/ics/kypo/userandgroup/config/DBModelConfig.java
@@ -4,9 +4,11 @@ package cz.muni.ics.kypo.userandgroup.config;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ComponentScan(basePackages = {"cz.muni.ics.kypo.userandgroup"})
 @EntityScan(basePackages = "cz.muni.ics.kypo.userandgroup.dbmodel")
+@PropertySource("file:${path-to-config-file}")
 public class DBModelConfig {
 }
