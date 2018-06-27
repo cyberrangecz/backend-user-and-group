7066c8a62e05d11ef0bdd3a5034fe9eb3403dd7c
Jan Duda <394179@mail.muni.cz>
2018-06-27 10:34:07 +0200

    Added authentication via OpenID Connect

---
+++ security/src/main/java/cz/muni/ics/kypo/userandgroup/security/config/SecurityConfig.java
@@ -0,0 +1,75 @@
+package cz.muni.ics.kypo.userandgroup.security.config;
+
+import org.mitre.oauth2.introspectingfilter.IntrospectingTokenService;
+import org.mitre.oauth2.introspectingfilter.service.impl.SimpleIntrospectionAuthorityGranter;
+import org.mitre.oauth2.introspectingfilter.service.impl.StaticIntrospectionConfigurationService;
+import org.mitre.oauth2.model.RegisteredClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
+
+import java.util.Set;
+
+@Configuration
+@EnableResourceServer
+@PropertySource("file:${path-to-config-file}")
+public class SecurityConfig extends ResourceServerConfigurerAdapter {
+
+    @Value("${kypo.idp.4oauth.introspectionURI}")
+    private String introspectionURI;
+
+    @Value("${kypo.idp.4oauth.clientId}")
+    private String clientId;
+
+    @Value("${kypo.idp.4oauth.clientSecret}")
+    private String clientSecret;
+
+    @Value("#{'${kypo.idp.4oauth.scopes}'.split(',')}")
+    private Set<String> scopes;
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) {
+        resources.tokenServices(tokenServices());
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated();
+    }
+
+    @Bean
+    public ResourceServerTokenServices tokenServices() {
+        IntrospectingTokenService tokenService = new IntrospectingTokenService();
+        tokenService.setIntrospectionConfigurationService(introspectionConfigurationService());
+        tokenService.setIntrospectionAuthorityGranter(introspectionAuthorityGranter());
+        return tokenService;
+    }
+
+    @Bean
+    public SimpleIntrospectionAuthorityGranter introspectionAuthorityGranter() {
+        return new SimpleIntrospectionAuthorityGranter();
+    }
+
+    @Bean
+    public StaticIntrospectionConfigurationService introspectionConfigurationService() {
+        StaticIntrospectionConfigurationService introspectionService = new StaticIntrospectionConfigurationService();
+        introspectionService.setIntrospectionUrl(introspectionURI);
+
+        RegisteredClient client = new RegisteredClient();
+        client.setClientId(clientId);
+        client.setClientSecret(clientSecret);
+        client.setScope(scopes);
+        introspectionService.setClientConfiguration(client);
+
+        return introspectionService;
+    }
+
+}
