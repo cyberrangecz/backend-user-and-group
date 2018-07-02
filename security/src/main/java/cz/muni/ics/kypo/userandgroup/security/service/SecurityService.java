5eb76dfafad39ada1730e70f59952be974747025
Jan Duda <394179@mail.muni.cz>
2018-07-02 15:09:09 +0200

    Added authorization in IDMGroupService

--- security/src/main/java/cz/muni/ics/kypo/userandgroup/security/service/SecurityService.java
+++ security/src/main/java/cz/muni/ics/kypo/userandgroup/security/service/SecurityService.java
@@ -1,7 +1,9 @@
 package cz.muni.ics.kypo.userandgroup.security.service;
 
 import com.google.gson.JsonObject;
+import cz.muni.ics.kypo.userandgroup.dbmodel.IDMGroup;
 import cz.muni.ics.kypo.userandgroup.dbmodel.User;
+import cz.muni.ics.kypo.userandgroup.persistence.IDMGroupRepository;
 import cz.muni.ics.kypo.userandgroup.persistence.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -13,23 +15,45 @@ public class SecurityService {
 
     private UserRepository userRepository;
 
+    private IDMGroupRepository groupRepository;
+
     @Autowired
-    public SecurityService(UserRepository userRepository) {
+    public SecurityService(UserRepository userRepository, IDMGroupRepository groupRepository) {
         this.userRepository = userRepository;
+        this.groupRepository = groupRepository;
     }
 
     public boolean hasLoggedInUserSameScreenName(String screenName) {
-        OAuth2Authentication authentication = (OAuth2Authentication) SecurityContextHolder.getContext().getAuthentication();
-        JsonObject credentials = (JsonObject) authentication.getUserAuthentication().getCredentials();
-
-        return screenName.equals(credentials.get("sub").getAsString());
+        return screenName.equals(getSubOfLoggedInUser());
     }
 
-    public boolean hasLoggedInUserSameId(Long id) {
+    public boolean hasLoggedInUserSameId(Long userId) {
+        User loggedInUser = getLoggedInUser();
+
+        return userId.equals(loggedInUser.getId());
+    }
+
+    public boolean isLoggedInUserInGroup(Long groupId) {
+        User loggedInUser = getLoggedInUser();
+        IDMGroup group = groupRepository.getOne(groupId);
+
+        return group.getUsers().contains(loggedInUser);
+    }
+
+    public boolean isLoggedInUserInGroup(String groupName) {
+        User loggedInUser = getLoggedInUser();
+        IDMGroup group = groupRepository.findByName(groupName);
+
+        return group.getUsers().contains(loggedInUser);
+    }
+
+    private User getLoggedInUser() {
+        return userRepository.findByScreenName(getSubOfLoggedInUser());
+    }
+
+    private String getSubOfLoggedInUser() {
         OAuth2Authentication authentication = (OAuth2Authentication) SecurityContextHolder.getContext().getAuthentication();
         JsonObject credentials = (JsonObject) authentication.getUserAuthentication().getCredentials();
-        User loggedInUser = userRepository.findByScreenName(credentials.get("sub").getAsString());
-
-        return id.equals(loggedInUser.getId());
+        return credentials.get("sub").getAsString();
     }
 }
