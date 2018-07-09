d26d0824c10f3f86819bae9ec850ecd224ab7bdb
Jan Duda <394179@mail.muni.cz>
2018-07-09 15:38:39 +0200

    Added loading users and assigning roles to them from external file

--- rest/src/main/java/cz/muni/ics/kypo/userandgroup/StartUpRunner.java
+++ rest/src/main/java/cz/muni/ics/kypo/userandgroup/StartUpRunner.java
@@ -7,6 +7,7 @@ import cz.muni.ics.kypo.userandgroup.dbmodel.*;
 import cz.muni.ics.kypo.userandgroup.persistence.IDMGroupRepository;
 import cz.muni.ics.kypo.userandgroup.persistence.RoleRepository;
 import cz.muni.ics.kypo.userandgroup.persistence.UserRepository;
+import org.hibernate.Hibernate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,11 +17,13 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
+import javax.transaction.Transactional;
 import java.io.File;
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @Component
 @PropertySource("file:${path-to-config-file}")
@@ -28,11 +31,8 @@ public class StartUpRunner implements ApplicationRunner {
 
     private static Logger LOGGER = LoggerFactory.getLogger(StartUpRunner.class);
 
-    @Value("${path.to.initial.users.file}")
-    private String pathToInitialUsersFile;
-
-    @Value("${path.to.initial.roles.file}")
-    private String pathToInitialRolesFile;
+    @Value("${path.to.file.with.initial.roles.and.users}")
+    private String pathToFileWithInitialRolesAndUsers;
 
     private UserRepository userRepository;
     private IDMGroupRepository groupRepository;
@@ -49,12 +49,54 @@ public class StartUpRunner implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
         ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
-        loadRoles(mapper);
+        JsonNode initialData = mapper.readTree(new File(pathToFileWithInitialRolesAndUsers));
+        loadRoles(initialData.get("roles"));
+        loadUsers(initialData.get("users"));
         LOGGER.info("Roles and users from external file were loaded and created in DB");
     }
 
-    private void loadRoles(ObjectMapper mapper) throws IOException {
-        JsonNode rolesOfServices = mapper.readTree(new File(pathToInitialRolesFile)).get("services");
+    private void loadUsers(JsonNode initialUsers) throws IOException {
+        Iterator<Map.Entry<String, JsonNode>> rolesWithUsersIterator = initialUsers.fields();
+        while (rolesWithUsersIterator.hasNext()) {
+            Map.Entry<String, JsonNode> usersOfRole = rolesWithUsersIterator.next();
+            if (roleRepository.existsByRoleType(usersOfRole.getKey())) {
+                LOGGER.info("Assigning role {} to users", usersOfRole.getKey());
+                Optional<IDMGroup> optioanlGroup = groupRepository.getIDMGroupByNameWithUsers(usersOfRole.getKey());
+                IDMGroup groupForRole = optioanlGroup.orElseThrow(IOException::new);
+                JsonNode usersJson = usersOfRole.getValue();
+
+                Iterator<JsonNode> usersJsonIterator = usersJson.elements();
+                while (usersJsonIterator.hasNext()) {
+                    JsonNode userJson = usersJsonIterator.next();
+                    User user = userRepository.findByScreenName(userJson.get("screenName").asText());
+                    if (user == null) {
+                        user = new User(userJson.get("screenName").asText());
+                        user.setFullName(userJson.get("fullName").asText());
+                        user.setMail(userJson.get("mail").asText());
+                        user.setStatus(UserAndGroupStatus.VALID);
+                        userRepository.saveAndFlush(user);
+                        LOGGER.warn("User with screeName {} was created and assigned role {}.", user.getScreenName(), usersOfRole.getKey());
+                        groupForRole.addUser(user);
+                    } else {
+                        if (!groupForRole.getUsers().contains(user)) {
+                            groupForRole.addUser(user);
+                            LOGGER.warn("User with screeName {} was assigned role {}.", user.getScreenName(), usersOfRole.getKey());
+                        } else {
+                            LOGGER.warn("SKIPPED, User with screeName {} already has role {}.", user.getScreenName(), usersOfRole.getKey());
+                        }
+                    }
+                }
+                groupRepository.save(groupForRole);
+                LOGGER.warn("{} role was given to users.", usersOfRole.getKey());
+            } else {
+                LOGGER.error("SKIPPED, Users with role {} were not loaded, the role does not exist.", usersOfRole.getKey());
+                throw new IOException("SKIPPED, Users with role " + usersOfRole.getKey() + " were not loaded, the role does not exist.");
+            }
+        }
+    }
+
+    private void loadRoles(JsonNode initialRoles) throws IOException {
+        JsonNode rolesOfServices = initialRoles.get("services");
 
         Iterator<JsonNode> services = rolesOfServices.elements();
         while (services.hasNext()) {
@@ -68,17 +110,18 @@ public class StartUpRunner implements ApplicationRunner {
                         roleRepository.save(newRole);
                         LOGGER.info("Role with name {} was created", roleName.asText());
 
-                        IDMGroup groupForRole = new IDMGroup("Group " + roleName.asText(),
+                        IDMGroup groupForRole = new IDMGroup(roleName.asText(),
                                 "Group of all users wiht role " + roleName.asText().toLowerCase());
                         groupForRole.addRole(newRole);
                         groupForRole.setStatus(UserAndGroupStatus.VALID);
                         groupRepository.save(groupForRole);
                         LOGGER.info("Group for role with name {} was created", roleName.asText());
                     } else {
-                        LOGGER.warn("Role with name {} already exists, it was not created.", roleName.asText());
+                        LOGGER.warn("SKIPPED, Role with name {} already exists.", roleName.asText());
                     }
                 });
             } else {
+                LOGGER.error("At least one service does not have name or roles parameter, please fix it and try it again");
                 throw new IOException("At least one service does not have name or roles parameter, please fix it and try it again");
             }
         }
