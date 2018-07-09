f0a5f4439f8b4a81f69ad58ad461e8afbbee33a1
Jan Duda <394179@mail.muni.cz>
2018-07-09 10:10:06 +0200

    Added loading roles from external file

--- rest/src/main/java/cz/muni/ics/kypo/userandgroup/StartUpRunner.java
+++ rest/src/main/java/cz/muni/ics/kypo/userandgroup/StartUpRunner.java
@@ -3,10 +3,7 @@ package cz.muni.ics.kypo.userandgroup;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
-import cz.muni.ics.kypo.userandgroup.dbmodel.IDMGroup;
-import cz.muni.ics.kypo.userandgroup.dbmodel.Role;
-import cz.muni.ics.kypo.userandgroup.dbmodel.RoleType;
-import cz.muni.ics.kypo.userandgroup.dbmodel.User;
+import cz.muni.ics.kypo.userandgroup.dbmodel.*;
 import cz.muni.ics.kypo.userandgroup.persistence.IDMGroupRepository;
 import cz.muni.ics.kypo.userandgroup.persistence.RoleRepository;
 import cz.muni.ics.kypo.userandgroup.persistence.UserRepository;
@@ -20,6 +17,8 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -50,24 +49,38 @@ public class StartUpRunner implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
         ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+        loadRoles(mapper);
+        LOGGER.info("Roles and users from external file were loaded and created in DB");
+    }
 
-        LOGGER.info("START");
-        try {
-//            Map<String, List<User>> rolesOfUser = mapper.readValue(new File(pathToInitialUsersFile), Map.class);
-            JsonNode jsonNode = mapper.readTree(new File(pathToInitialUsersFile));
+    private void loadRoles(ObjectMapper mapper) throws IOException {
+        JsonNode rolesOfServices = mapper.readTree(new File(pathToInitialRolesFile)).get("services");
 
-            LOGGER.info("Users loaded");
-            LOGGER.info(jsonNode.toString());
-            jsonNode.fields().forEachRemaining(stringJsonNodeEntry -> LOGGER.info(stringJsonNodeEntry.toString()));
-            jsonNode.fields().forEachRemaining(stringJsonNodeEntry -> {
-                if(stringJsonNodeEntry.getKey().toUpperCase().equals(RoleType.ADMINISTRATOR.name())) {
-                    Role adminRole = roleRepository.findByRoleType(RoleType.ADMINISTRATOR.name());
-                }
-            });
+        Iterator<JsonNode> services = rolesOfServices.elements();
+        while (services.hasNext()) {
+            JsonNode service = services.next();
+            if (service.has("name") && service.has("roles")) {
+                JsonNode roles = service.get("roles");
+                roles.forEach(roleName -> {
+                    if (!roleRepository.existsByRoleType(roleName.asText())) {
+                        Role newRole = new Role();
+                        newRole.setRoleType(roleName.asText());
+                        roleRepository.save(newRole);
+                        LOGGER.info("Role with name {} was created", roleName.asText());
 
-        } catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+                        IDMGroup groupForRole = new IDMGroup("Group " + roleName.asText(),
+                                "Group of all users wiht role " + roleName.asText().toLowerCase());
+                        groupForRole.addRole(newRole);
+                        groupForRole.setStatus(UserAndGroupStatus.VALID);
+                        groupRepository.save(groupForRole);
+                        LOGGER.info("Group for role with name {} was created", roleName.asText());
+                    } else {
+                        LOGGER.warn("Role with name {} already exists, it was not created.", roleName.asText());
+                    }
+                });
+            } else {
+                throw new IOException("At least one service does not have name or roles parameter, please fix it and try it again");
+            }
         }
     }
 }
