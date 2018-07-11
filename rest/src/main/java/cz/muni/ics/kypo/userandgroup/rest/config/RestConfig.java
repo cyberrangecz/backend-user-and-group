6312b580ad24b99a9d7b24152927a131bd9032db
Jan Duda <394179@mail.muni.cz>
2018-07-11 09:56:38 +0200

    Added object mapper and jackson converter

--- rest/src/main/java/cz/muni/ics/kypo/userandgroup/rest/config/RestConfig.java
+++ rest/src/main/java/cz/muni/ics/kypo/userandgroup/rest/config/RestConfig.java
@@ -1,53 +1,47 @@
 package cz.muni.ics.kypo.userandgroup.rest.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import cz.muni.ics.kypo.userandgroup.config.ServiceConfig;
-import cz.muni.ics.kypo.userandgroup.security.config.ResourceServerSecurityConfig;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
 
-//@SpringBootApplication
-//@EnableSpringDataWebSupport
+@EnableSpringDataWebSupport
 @Import({ServiceConfig.class, SwaggerConfig.class})
 @ComponentScan(basePackages = {"cz.muni.ics.kypo.userandgroup.rest.mapping"})
-public class RestConfig {// extends SpringBootServletInitializer {
+public class RestConfig {
 
-//    @Override
-//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-//        return application.sources(RestConfig.class);
-//    }
-//
-//    public static void main(String[] args) {
-//        SpringApplication.run(RestConfig.class, args);
-//    }
-//
-//    @Bean
-//    @Primary
-//    public MappingJackson2HttpMessageConverter jacksonHTTPMessageConverter() {
-//        MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
-//        jsonConverter.setObjectMapper(objectMapperForRestAPI());
-//        return jsonConverter;
-//    }
-//
-//    @Bean(name = "objMapperRESTApi")
-//    @Primary
-//    public ObjectMapper objectMapperForRestAPI() {
-//        ObjectMapper obj = new ObjectMapper();
-//        obj.setPropertyNamingStrategy(snakeCase());
-//        return obj;
-//    }
-//
-//    /**
-//     * Naming strategy for returned JSONs.
-//     *
-//     * @return Naming Strategy for JSON properties
-//     */
-//    @Bean(name = "properyNamingSnakeCase")
-//    public PropertyNamingStrategy snakeCase() {
-//        return PropertyNamingStrategy.SNAKE_CASE;
-//    }
+    @Bean
+    @Primary
+    public MappingJackson2HttpMessageConverter jacksonHTTPMessageConverter() {
+        MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
+        jsonConverter.setObjectMapper(objectMapperForRestAPI());
+        return jsonConverter;
+    }
+
+    @Bean(name = "objMapperRESTApi")
+    @Primary
+    public ObjectMapper objectMapperForRestAPI() {
+        ObjectMapper obj = new ObjectMapper();
+        obj.setPropertyNamingStrategy(snakeCase());
+        return obj;
+    }
+
+    /**
+     * Naming strategy for returned JSONs.
+     *
+     * @return Naming Strategy for JSON properties
+     */
+    @Bean(name = "properyNamingSnakeCase")
+    public PropertyNamingStrategy snakeCase() {
+        return PropertyNamingStrategy.SNAKE_CASE;
+    }
 
     @Bean
     public ModelMapper modelMapper() {
