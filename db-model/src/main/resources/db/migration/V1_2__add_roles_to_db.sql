db8f38406a9730b33b1efb8dd61302bfa3a96260
Jan Duda <394179@mail.muni.cz>
2018-07-16 14:47:26 +0200

    Added inserting groups for main roles

--- db-model/src/main/resources/db/migration/V1_2__add_roles_to_db.sql
+++ db-model/src/main/resources/db/migration/V1_2__add_roles_to_db.sql
@@ -1,4 +1,14 @@
 -- inserting main roles
-INSERT INTO role(role_type) VALUES ('ADMINISTRATOR');
-INSERT INTO role(role_type) VALUES ('USER');
-INSERT INTO role(role_type) VALUES ('GUEST');
+INSERT INTO role (role_type) VALUES ('ADMINISTRATOR');
+INSERT INTO role (role_type) VALUES ('USER');
+INSERT INTO role (role_type) VALUES ('GUEST');
+
+-- inserting groups for main roles
+INSERT INTO idm_group (name, status, description) VALUES ('ADMINISTRATOR', 'VALID', 'Initial group for users with ADMINISTRATOR role');
+INSERT INTO idm_group (name, status, description) VALUES ('USER', 'VALID', 'Initial group for users with USER role');
+INSERT INTO idm_group (name, status, description) VALUES ('GUEST', 'VALID', 'Initial group for users with GUEST role');
+
+-- connecting main roles with groups
+INSERT INTO idm_group_role (role_id, idm_group_id) VALUES ((SELECT id FROM role WHERE role_type = 'ADMINISTRATOR'), (SELECT id FROM idm_group WHERE name = 'ADMINISTRATOR'));
+INSERT INTO idm_group_role (role_id, idm_group_id) VALUES ((SELECT id FROM role WHERE role_type = 'USER'), (SELECT id FROM idm_group WHERE name = 'USER'));
+INSERT INTO idm_group_role (role_id, idm_group_id) VALUES ((SELECT id FROM role WHERE role_type = 'GUEST'), (SELECT id FROM idm_group WHERE name = 'GUEST'));
