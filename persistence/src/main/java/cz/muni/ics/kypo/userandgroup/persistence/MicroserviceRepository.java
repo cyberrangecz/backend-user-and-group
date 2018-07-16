e36d3a339b6e3fedc367262b0b5304a0c6ef4236
Jan Duda <394179@mail.muni.cz>
2018-07-16 13:49:02 +0200

    Added repository for microservice

--- persistence/src/main/java/cz/muni/ics/kypo/userandgroup/persistence/MicroserviceRepository.java
+++ persistence/src/main/java/cz/muni/ics/kypo/userandgroup/persistence/MicroserviceRepository.java
@@ -1,4 +1,11 @@
 package cz.muni.ics.kypo.userandgroup.persistence;
 
-public class MicroserviceRepository {
+import cz.muni.ics.kypo.userandgroup.dbmodel.Microservice;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MicroserviceRepository extends JpaRepository<Microservice, Long> {
+
+    Optional<Microservice> findByName(String name);
 }
--- persistence/src/test/java/cz/muni/ics/kypo/userandgroup/persistence/MicroserviceRepositoryTest.java
+++ persistence/src/test/java/cz/muni/ics/kypo/userandgroup/persistence/MicroserviceRepositoryTest.java
@@ -1,4 +1,51 @@
 package cz.muni.ics.kypo.userandgroup.persistence;
 
+import cz.muni.ics.kypo.userandgroup.dbmodel.Microservice;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@EntityScan(basePackages = {"cz.muni.ics.kypo.userandgroup.dbmodel"})
 public class MicroserviceRepositoryTest {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private MicroserviceRepository microserviceRepository;
+
+    private String name = "training";
+    private String endpoint = "/training";
+
+    @SpringBootApplication
+    static class TestConfiguration {
+    }
+
+    @Test
+    public void findByName() throws Exception {
+        Microservice microservice = new Microservice(name, endpoint);
+        this.entityManager.persist(microservice);
+        Optional<Microservice> optionalMicroservice = this.microserviceRepository.findByName(name);
+        Microservice m = optionalMicroservice.orElseThrow(() -> new Exception("Microservice shoul be found"));
+        assertEquals(microservice, m);
+        assertEquals(microservice.getName(), m.getName());
+        assertEquals(microservice.getEndpoint(), m.getEndpoint());
+    }
+
+    @Test
+    public void findByNameNotFound() {
+        assertFalse(this.microserviceRepository.findByName("not existing microservice").isPresent());
+    }
 }
