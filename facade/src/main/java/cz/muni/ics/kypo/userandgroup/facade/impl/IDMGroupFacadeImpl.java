a731f1ebdf5d219511b6b3003bbbf0e6dd2a3087
Jan Duda <394179@mail.muni.cz>
2018-07-20 12:28:07 +0200

    Implemented IDMGroupFacade in facade

--- facade/src/main/java/cz/muni/ics/kypo/userandgroup/facade/impl/IDMGroupFacadeImpl.java
+++ facade/src/main/java/cz/muni/ics/kypo/userandgroup/facade/impl/IDMGroupFacadeImpl.java
@@ -1,69 +1,116 @@
 package cz.muni.ics.kypo.userandgroup.facade.impl;
 
 import com.querydsl.core.types.Predicate;
+import cz.muni.ics.kypo.userandgroup.api.PageResultResource;
 import cz.muni.ics.kypo.userandgroup.api.dto.group.*;
 import cz.muni.ics.kypo.userandgroup.api.dto.role.RoleDTO;
+import cz.muni.ics.kypo.userandgroup.exception.IdentityManagementException;
 import cz.muni.ics.kypo.userandgroup.facade.interfaces.IDMGroupFacade;
+import cz.muni.ics.kypo.userandgroup.mapping.BeanMapping;
+import cz.muni.ics.kypo.userandgroup.model.IDMGroup;
+import cz.muni.ics.kypo.userandgroup.model.Role;
 import cz.muni.ics.kypo.userandgroup.model.RoleType;
+import cz.muni.ics.kypo.userandgroup.service.interfaces.IDMGroupService;
+import cz.muni.ics.kypo.userandgroup.util.GroupDeletionStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
+@Service
+@Transactional
 public class IDMGroupFacadeImpl implements IDMGroupFacade {
+
+    private IDMGroupService groupService;
+    private BeanMapping beanMapping;
+
+    @Autowired
+    public IDMGroupFacadeImpl(IDMGroupService groupService, BeanMapping beanMapping) {
+        this.groupService = groupService;
+        this.beanMapping = beanMapping;
+    }
 
     @Override
     public GroupDTO createGroup(NewGroupDTO newGroupDTO) {
-        return null;
+        IDMGroup group = beanMapping.mapTo(newGroupDTO, IDMGroup.class);
+        GroupDTO createdGroup = beanMapping.mapTo(groupService.create(group), GroupDTO.class);
+        return createdGroup;
     }
 
     @Override
     public GroupDTO updateGroup(UpdateGroupDTO updateGroupDTO) {
-        return null;
+        IDMGroup group = beanMapping.mapTo(updateGroupDTO, IDMGroup.class);
+        GroupDTO updatedGroup = beanMapping.mapTo(groupService.update(group), GroupDTO.class);
+        return updatedGroup;
     }
 
     @Override
     public GroupDTO removeMembers(Long groupId, List<Long> userIds) {
-        return null;
+        GroupDTO updatedGroup = beanMapping.mapTo(groupService.removeMembers(groupId, userIds), GroupDTO.class);
+        return updatedGroup;
     }
 
     @Override
     public GroupDTO addMembers(AddMembersToGroupDTO addMembers) {
-        return null;
+        IDMGroup group = groupService.addMembers(addMembers.getGroupId(),
+                addMembers.getIdsOfGroupsOfImportedUsers(), addMembers.getIdsOfUsersToBeAdd());
+        GroupDTO updatedGroup = beanMapping.mapTo(group, GroupDTO.class);
+        return updatedGroup;
     }
 
     @Override
     public GroupDeletionResponseDTO deleteGroup(Long id) {
-        return null;
+        IDMGroup group = groupService.get(id);
+        GroupDeletionStatus deletionStatus = groupService.delete(group);
+        GroupDeletionResponseDTO groupDeletionResponseDTO = beanMapping.mapTo(group, GroupDeletionResponseDTO.class);
+        groupDeletionResponseDTO.setStatus(deletionStatus);
+        return groupDeletionResponseDTO;
     }
 
     @Override
     public List<GroupDeletionResponseDTO> deleteGroups(List<Long> ids) {
-        return null;
+        Map<IDMGroup, GroupDeletionStatus> mapOfResults = groupService.deleteGroups(ids);
+        List<GroupDeletionResponseDTO> response = new ArrayList<>();
+
+        mapOfResults.forEach((group, status) -> {
+            GroupDeletionResponseDTO r = new GroupDeletionResponseDTO();
+            r.setId(group.getId());
+            r.setName(group.getName());
+            r.setStatus(status);
+            response.add(r);
+        });
+        return response;
     }
 
     @Override
-    public List<GroupDTO> getAllGroups(Predicate predicate, Pageable pageable) {
-        return null;
+    public PageResultResource<GroupDTO> getAllGroups(Predicate predicate, Pageable pageable) {
+        Page<IDMGroup> groups = groupService.getAllIDMGroups(predicate, pageable);
+        return beanMapping.mapToPageResultDTO(groups, GroupDTO.class);
     }
 
     @Override
     public GroupDTO getGroup(Long id) {
-        return null;
+        return beanMapping.mapTo(groupService.get(id), GroupDTO.class);
     }
 
     @Override
     public Set<RoleDTO> getRolesOfGroup(Long id) {
-        return null;
+        return beanMapping.mapToSet(groupService.getRolesOfGroup(id), RoleDTO.class);
     }
 
     @Override
     public GroupDTO assignRole(Long groupId, RoleType roleType) {
-        return null;
+        return beanMapping.mapTo(groupService.assignRole(groupId, roleType), GroupDTO.class);
     }
 
     @Override
     public GroupDTO assignRoleInMicroservice(Long groupId, Long roleId, Long microserviceId) {
-        return null;
+        return beanMapping.mapTo(groupService.assignRoleInMicroservice(groupId, roleId, microserviceId), GroupDTO.class);
     }
 }
--- facade/src/main/java/cz/muni/ics/kypo/userandgroup/facade/interfaces/IDMGroupFacade.java
+++ facade/src/main/java/cz/muni/ics/kypo/userandgroup/facade/interfaces/IDMGroupFacade.java
@@ -1,6 +1,7 @@
 package cz.muni.ics.kypo.userandgroup.facade.interfaces;
 
 import com.querydsl.core.types.Predicate;
+import cz.muni.ics.kypo.userandgroup.api.PageResultResource;
 import cz.muni.ics.kypo.userandgroup.api.dto.group.*;
 import cz.muni.ics.kypo.userandgroup.api.dto.role.RoleDTO;
 import cz.muni.ics.kypo.userandgroup.model.RoleType;
@@ -11,25 +12,96 @@ import java.util.Set;
 
 public interface IDMGroupFacade {
 
+    /**
+     * Creates IDMGroup with informetion from given newGroupDTO and returns created group.
+     *
+     * @param newGroupDTO group to be created
+     * @return created group
+     */
     GroupDTO createGroup(NewGroupDTO newGroupDTO);
 
+    /**
+     * Updates given IDM group in database.
+     *
+     * @param updateGroupDTO group to be updated
+     * @return updated group
+     */
     GroupDTO updateGroup(UpdateGroupDTO updateGroupDTO);
 
+    /**
+     * Removes members of group with given userIds from the group
+     *
+     * @param groupId id of group from which given users should be removed
+     * @param userIds ids of users to be removed from given group
+     * @return updated group
+     */
     GroupDTO removeMembers(Long groupId, List<Long> userIds);
 
+    /**
+     * Adds users and users from groups to group with given groupId in input parameter addMember
+     *
+     * @param addMembers parameter containing users to be added, groups which users will be added to group with groupId which is also specify in this parameter
+     * @return updated group
+     */
     GroupDTO addMembers(AddMembersToGroupDTO addMembers);
 
+    /**
+     * Deletes group with given id from database and returns status of deletion with group name and id.
+     *
+     * @param id of group to be deleted
+     * @return status of deletion with name and id of group
+     */
     GroupDeletionResponseDTO deleteGroup(Long id);
 
+    /**
+     * Deletes groups with given ids from database and returns statuses of deletion with groups name and id.
+     *
+     * @param ids of groups to be deleted
+     * @return statuses of deletion with name and id of groups
+     */
     List<GroupDeletionResponseDTO> deleteGroups(List<Long> ids);
 
-    List<GroupDTO> getAllGroups(Predicate predicate, Pageable pageable);
+    /**
+     * Returns page of groups specified by given predicate and pageable
+     *
+     * @param predicate specifies query to databse
+     * @param pageable parameter with information about pagination
+     * @return page of groups specified by given predicate and pageable
+     */
+    PageResultResource<GroupDTO> getAllGroups(Predicate predicate, Pageable pageable);
 
+    /**
+     * Returns group with given id
+     *
+     * @param id of group to be loaded
+     * @return group with given id
+     */
     GroupDTO getGroup(Long id);
 
+    /**
+     * Returns all roles from all registered microservices of group with given id
+     *
+     * @param id of group
+     * @return all roles of group with given id
+     */
     Set<RoleDTO> getRolesOfGroup(Long id);
 
+    /**
+     * Assigns role with given roleType to group with given groupId in this microservice
+     *
+     * @param groupId of group
+     * @param roleType to be assigned to group
+     * @return updated group
+     */
     GroupDTO assignRole(Long groupId, RoleType roleType);
 
+    /**
+     * Assigns role with given roleId in microservice with microserviceID to group with given groupId
+     *
+     * @param groupId of group
+     * @param roleId of role to be assigned to group from specific microservice
+     * @param microserviceId of microservice from which is role
+     * @return updated group
+     */
     GroupDTO assignRoleInMicroservice(Long groupId, Long roleId, Long microserviceId);
 }
