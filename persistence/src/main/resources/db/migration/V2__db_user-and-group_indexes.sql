d56e75926c6eb657b0fdb12596e337919fab18ee
Pavel Šeda <441048@mail.muni.cz>
2019-05-05 17:08:14 +0000

    change some indexes to unique indexes.

--- persistence/src/main/resources/db/migration/V2__db_user-and-group_indexes.sql
+++ persistence/src/main/resources/db/migration/V2__db_user-and-group_indexes.sql
@@ -1,13 +1,13 @@
-CREATE INDEX idm_group_name_index
+CREATE UNIQUE INDEX idm_group_name_index
 ON idm_group (name);
 
-CREATE INDEX microservice_name_index
+CREATE UNIQUE INDEX microservice_name_index
 ON microservice (name);
 
-CREATE INDEX role_role_type_index
+CREATE UNIQUE INDEX role_role_type_index
 ON role (role_type);
 
-CREATE INDEX user_login_index
+CREATE UNIQUE INDEX user_login_index
 ON users (login);
 
 CREATE INDEX user_full_name_index
