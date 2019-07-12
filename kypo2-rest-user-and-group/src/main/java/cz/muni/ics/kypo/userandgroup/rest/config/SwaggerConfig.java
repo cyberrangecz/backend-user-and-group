762f68e1c25c7d9e0d5eb7c32d497ac8eb2a7560
Dominik Pilár <445537@mail.muni.cz>
2019-07-12 10:42:21 +0000

    Resolve "Running multiple OIDC providers in parallel"

--- kypo2-rest-user-and-group/src/main/java/cz/muni/ics/kypo/userandgroup/rest/config/SwaggerConfig.java
+++ kypo2-rest-user-and-group/src/main/java/cz/muni/ics/kypo/userandgroup/rest/config/SwaggerConfig.java
@@ -23,13 +23,13 @@ import java.util.*;
 @EnableSwagger2
 public class SwaggerConfig {
 
-    @Value("${kypo.idp.4oauth.authorizationURI}")
+    @Value("${kypo.muni.idp.4oauth.authorizationURI}")
     private String authorizationURI;
 
-    @Value("${kypo.idp.4oauth.client.clientId}")
+    @Value("${kypo.muni.idp.4oauth.client.clientId}")
     private String clientIdOfClient;
 
-    @Value("#{'${kypo.idp.4oauth.scopes}'.split(',')}")
+    @Value("#{'${kypo.muni.idp.4oauth.scopes}'.split(',')}")
     private Set<String> scopes;
 
     private static String NAME_OF_TOKEN = "bearer";
