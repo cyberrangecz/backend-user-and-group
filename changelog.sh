42aacb311d5c1042ceb3aec9ed384d1bcb8914b0
Pavel Šeda <441048@mail.muni.cz>
2020-03-15 15:05:23 +0000

    Delete changelog.sh

--- changelog.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-firstTag=$(git tag | sort -r | head -1)
-secondTag=$(git tag | sort -r | head -2 | awk '{split($0, tags, "\n")} END {print tags[1]}')
-echo "Changes between ${secondTag} and ${firstTag}\n"
-git log  --pretty=format:' * %s' ${secondTag}..${firstTag}
