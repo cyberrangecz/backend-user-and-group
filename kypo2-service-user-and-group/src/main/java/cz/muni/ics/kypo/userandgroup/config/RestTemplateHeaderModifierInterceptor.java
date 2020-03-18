c53e8a1eeac11ab44208edf97301170a6c62cf8c
SedaQ <pavelseda@email.cz>
2020-03-18 21:49:17 +0100

    refactor PathVariable names to be consistent throughout the API.

--- kypo2-service-user-and-group/src/main/java/cz/muni/ics/kypo/userandgroup/config/RestTemplateHeaderModifierInterceptor.java
+++ kypo2-service-user-and-group/src/main/java/cz/muni/ics/kypo/userandgroup/config/RestTemplateHeaderModifierInterceptor.java
@@ -1,6 +1,7 @@
 package cz.muni.ics.kypo.userandgroup.config;
 
 import org.springframework.http.HttpRequest;
+import org.springframework.http.MediaType;
 import org.springframework.http.client.ClientHttpRequestExecution;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.http.client.ClientHttpResponse;
@@ -10,6 +11,7 @@ import org.springframework.security.oauth2.provider.authentication.OAuth2Authent
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  * That class adds HTTP Authorization header to all rest template calls.
@@ -22,6 +24,8 @@ public class RestTemplateHeaderModifierInterceptor implements ClientHttpRequestI
         OAuth2Authentication authenticatedUser = (OAuth2Authentication) SecurityContextHolder.getContext().getAuthentication();
         OAuth2AuthenticationDetails details = (OAuth2AuthenticationDetails) authenticatedUser.getDetails();
         request.getHeaders().add("Authorization", "Bearer " + details.getTokenValue());
+        request.getHeaders().setAccept(List.of(MediaType.APPLICATION_JSON));
+        request.getHeaders().setContentType(MediaType.APPLICATION_JSON);
         return execution.execute(request, body);
     }
 }
