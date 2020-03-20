07bf058b1f8d7c5c9456d276c33b3c34adefb593
Dominik Pilar <xpilar3@fi.muni.cz>
2020-03-20 08:46:01 +0100

    Removed RestTemplateConfiguration and rest template interceptor

--- kypo2-service-user-and-group/src/main/java/cz/muni/ics/kypo/userandgroup/config/ServiceConfig.java
+++ kypo2-service-user-and-group/src/main/java/cz/muni/ics/kypo/userandgroup/config/ServiceConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.*;
 
 @Configuration
 @EnableCaching
-@Import({PersistenceConfig.class, ValidationMessagesConfig.class, RestTemplateConfiguration.class})
+@Import({PersistenceConfig.class, ValidationMessagesConfig.class})
 @ComponentScan(basePackages = {"cz.muni.ics.kypo.userandgroup.service", "cz.muni.ics.kypo.userandgroup.config", "cz.muni.ics.kypo.userandgroup.facade", "cz.muni.ics.kypo.userandgroup.mapping", "cz.muni.ics.kypo.userandgroup.startuprunners"})
 public class ServiceConfig {
 
