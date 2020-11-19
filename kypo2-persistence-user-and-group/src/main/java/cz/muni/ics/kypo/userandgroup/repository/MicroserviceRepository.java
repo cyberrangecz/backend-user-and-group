19938056d0d482df83006e3d581688bd928cf05f
Dominik Pilár <xpilar3@fi.muni.cz>
2020-11-19 13:18:51 +0100

    Added support of filtering microservices by name.

--- kypo2-persistence-user-and-group/src/main/java/cz/muni/ics/kypo/userandgroup/repository/MicroserviceRepository.java
+++ kypo2-persistence-user-and-group/src/main/java/cz/muni/ics/kypo/userandgroup/repository/MicroserviceRepository.java
@@ -1,16 +1,38 @@
 package cz.muni.ics.kypo.userandgroup.repository;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.StringPath;
 import cz.muni.ics.kypo.userandgroup.entities.Microservice;
+import cz.muni.ics.kypo.userandgroup.entities.QIDMGroup;
+import cz.muni.ics.kypo.userandgroup.entities.QMicroservice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 
+import java.util.Collection;
 import java.util.Optional;
 
 /**
  * The JPA repository interface to manage {@link Microservice} instances.
  */
-public interface MicroserviceRepository extends JpaRepository<Microservice, Long>, QuerydslPredicateExecutor<Microservice> {
-    
+public interface MicroserviceRepository extends JpaRepository<Microservice, Long>, QuerydslPredicateExecutor<Microservice>, QuerydslBinderCustomizer<QMicroservice> {
+
+    /**
+     * That method is used to make the query dsl string values case insensitive
+     *
+     * @param querydslBindings
+     * @param qMicroservice
+     */
+    @Override
+    default void customize(QuerydslBindings querydslBindings, QMicroservice qMicroservice) {
+        querydslBindings.bind(String.class).all((StringPath path, Collection<? extends String> values) -> {
+            BooleanBuilder predicate = new BooleanBuilder();
+            values.forEach(value -> predicate.and(path.containsIgnoreCase(value)));
+            return Optional.ofNullable(predicate);
+        });
+    }
+
     /**
      * Find Microservice by its name.
      *
